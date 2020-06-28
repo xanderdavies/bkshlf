fixed rotation

main.py
@@ -14,6 +14,7 @@
 import cv2 as cv
 import sys
 import numpy as np
+import imutils
 
 
 def scaleToLimit(image, rowLimit):
@@ -24,16 +25,20 @@ def scaleToLimit(image, rowLimit):
     return image;
 
 image = cv.imread(sys.argv[1], cv.IMREAD_COLOR)
-image = scaleToLimit(image, 800)
+image = scaleToLimit(image, 400)
 
 def rotateMatrix(im, degrees):
     rows = im.shape[0]
     cols = im.shape[1]
     M = cv.getRotationMatrix2D((cols/2,rows/2),degrees,1)
-    dst = cv.warpAffine(im, M, ((int) (np.cos(degrees) * (rows + cols)), (int) (np.sin(degrees) * (rows + cols))))
+    rads = degrees / 180 * np.pi
+    new_size = ((int) (np.sin(rads) * rows + np.cos(rads) * cols), (int) (np.cos(rads) * rows + np.sin(rads) * cols))
+    print(new_size)
+    print(np.cos(rads), np.sin(rads), rows, cols)
+    dst = cv.warpAffine(im, M, new_size)
     return dst
 
-image = rotateMatrix(image, 30)
+image = imutils.rotate_bound(image, 30) #rotateMatrix(image, 30)
 cv.imshow("original", image)
 # cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 # cannyOutput = cv.canny(image)